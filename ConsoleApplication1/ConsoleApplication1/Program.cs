89a444c0e2f1a7e2dd113752961fa85706f882fa
Danilo <Danilo@QBEX>
2017-04-03 09:04:09 -0300

    segundo teste

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //teste
             Console.WriteLine("bfsb");
+            Console.ReadLine();
         }
     }
 }
